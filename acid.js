5e842b59dcfbe2b4f16e0c75b1942d0b13385c9c
Kyle Watson <kylewatson98@gmail.com>
2014-04-08 07:51:20 +0100

    Platforms and cameras
    Added platform physics (can jump through blocks side ways and up through
    them but not down through them) Also started adding camera object to
    orientate the screen around a display child - broke - left and up go way
    too quick

--- acid.js
+++ acid.js
@@ -30,10 +30,10 @@ window.onload = function(){
 	acid.height = mainCanvas.height;
 	frame = mainCanvas.getContext("2d");
 	acid.debug = false;
-	acid.keyboard;
-	acid.keyboard;
+	acid.keyboard = {right:false, left:false, up:false, down:false}
 	document.body.onkeydown = keydown;
 	document.body.onkeyup = keyup;
+	acid.camera = {xs:0, ys:0, id:-1, margin:10};
 	acid.init();
 	if(!acid.debug){
 		document.body.removeChild(outputBox);
@@ -46,15 +46,46 @@ window.onload = function(){
 }
 
 function keydown(evt){
+	switch(evt.keyCode){
+		case 39:
+			acid.keyboard.right = true;
+		break;
+		case 37:
+			acid.keyboard.left = true;
+		break;
+		case 38:
+			acid.keyboard.up = true;
+		break;
+		case 40:
+			acid.keyboard.down = true;
+		break;
+		default:
+			//
+	}
 	acid.keydown(evt.keyCode);
-	acid.output(acid.length);
 }
 
 function keyup(evt){
+	switch(evt.keyCode){
+		case 39:
+			acid.keyboard.right = false;
+		break;
+		case 37:
+			acid.keyboard.left = false;
+		break;
+		case 38:
+			acid.keyboard.up = false;
+		break;
+		case 40:
+			acid.keyboard.down = false;
+		break;
+		default:
+			//
+	}
 	acid.keyup(evt.keyCode);
 }
 
-acid.rect = function(ftype, wid, hig, col){
+acid.rect = function(ftype, wid, hig, col, platform){
 	shapeCanvas = document.createElement("canvas");
 	shapeCanvas.width = wid;
 	shapeCanvas.height = hig;
@@ -95,7 +126,7 @@ function updateThings(){
 							}
 						}
 						else{
-							physicsList[i].velX += physicsList[i].friction;		
+							physicsList[i].velX += physicsList[i].friction + physicsList[i2].friction;		
 							if(physicsList[i].velX > 0.1 && physicsList[i].velX != 0){
 								physicsList[i].velX = 0;
 							}						
@@ -109,7 +140,7 @@ function updateThings(){
 							}
 						}
 						else{
-							physicsList[i].velY += physicsList[i].friction;
+							physicsList[i].velY += physicsList[i].friction + physicsList[i2].friction;
 							if(physicsList[i].velY > 0.1 && physicsList[i].velY != 0){
 								physicsList[i].velY = 0;
 							}
@@ -132,68 +163,111 @@ function updateThings(){
 	}
 	
 	for(var i=0;i<displayList.length;i++){
-		frame.drawImage(displayList[i], displayList[i].xs, displayList[i].ys);
+		if(acid.camera.id == i){
+			if(displayList[i].xs + displayList[i].nwidth + acid.camera.margin > acid.width){
+				acid.camera.xs -= displayList[i].xs + displayList[i].nwidth + acid.camera.margin - acid.width;
+				displayList[i].xs = acid.width - displayList[i].nwidth - acid.camera.margin;				
+			}
+			else if(displayList[i].xs - acid.camera.margin < 0){
+				acid.camera.xs += displayList[i].xs;
+				displayList[i].xs = acid.camera.margin;				
+			}
+			if(displayList[i].ys + displayList[i].nheight + acid.camera.margin > acid.height){
+				acid.camera.ys -= displayList[i].ys + displayList[i].nheight + acid.camera.margin - acid.height;
+				displayList[i].ys = acid.height - displayList[i].nheight - acid.camera.margin;				
+			}
+			else if(displayList[i].ys - acid.camera.margin < 0){
+				acid.camera.ys += displayList[i].ys;
+				displayList[i].ys = acid.camera.margin;	
+			}
+			frame.drawImage(displayList[i], displayList[i].xs, displayList[i].ys);
+			frame.strokeRect(acid.camera.margin, acid.camera.margin, acid.width - 2*acid.camera.margin, acid.height - 2*acid.camera.margin);
+		}
+		else{
+			frame.drawImage(displayList[i], displayList[i].xs + acid.camera.xs, displayList[i].ys + acid.camera.ys);
+		}
 	}
 }
 
 acid.checkcol = function(obj1, obj2, fric, p){
 	if(physicsList.indexOf(obj1) > -1 && physicsList.indexOf(obj2) > -1 ){
-		left1 = obj1.xs + obj1.boundBox[0];
-		left2 = obj2.xs + obj2.boundBox[0];
-		top1 = obj1.ys + obj1.boundBox[1];
-		top2 = obj2.ys + obj2.boundBox[1];
-		right1 = obj1.xs + obj1.boundBox[2];
-		right2 = obj2.xs + obj2.boundBox[2];
-		bottom1 = obj1.ys + obj1.boundBox[3];
-		bottom2 = obj2.ys + obj2.boundBox[3];
+		if(obj1.camera){
+			left1 = obj1.xs + obj1.boundBox[0];
+			top1 = obj1.ys + obj1.boundBox[1];
+			right1 = obj1.xs + obj1.boundBox[2];
+			bottom1 = obj1.ys + obj1.boundBox[3];
+		}
+		else{
+			left1 = obj1.xs + obj1.boundBox[0] + acid.camera.xs;
+			top1 = obj1.ys + obj1.boundBox[1] + acid.camera.ys;
+			right1 = obj1.xs + obj1.boundBox[2] + acid.camera.xs;
+			bottom1 = obj1.ys + obj1.boundBox[3] + acid.camera.ys;
+		}
+		left2 = obj2.xs + obj2.boundBox[0] + acid.camera.xs;
+		top2 = obj2.ys + obj2.boundBox[1] + acid.camera.ys;
+		right2 = obj2.xs + obj2.boundBox[2] + acid.camera.xs;
+		bottom2 = obj2.ys + obj2.boundBox[3] + acid.camera.ys;
 		
-		if(p){
-			if(!fric){
-				if(!(right1 + obj1.velX	< left2 || left1 + obj1.velX > right2 || bottom1 < top2 || top1 > bottom2)){
-					return true;
-				}
-				else if(right1 < left2 && left1 + obj1.velX > right2 || left1 > right2 && right1 + obj1.velX < left2){
-					return true;			
-				}
-				else{
-					return false;
-				}
+		if(obj2.platform){
+			if(p &! fric){
+				return false;
+			}
+			else if(!(right1 < left2 || left1 > right2 || bottom1 + obj1.velY < top2 || top1 + obj1.velY > bottom2) && Math.abs(obj1.velY) == obj1.velY && bottom1 < top2){
+				return true;
 			}
 			else{
-				if(!(right1 + obj1.velX	< left2 || left1 + obj1.velX > right2 || bottom1 + 1 < top2 || top1 - 1 > bottom2)){
-					return true;
-				}
-				else{
-					return false;
-				}
+				return false;
 			}
 		}
 		else{
-			if(!fric){
-				if(!(right1 < left2 || left1 > right2 || bottom1 + obj1.velY < top2 || top1 + obj1.velY > bottom2)){
-					return true;
-				}
-				else if(bottom1 < top2 && top1 + obj1.velY > bottom2 || top1 > bottom2 && bottom1 + obj1.velY < top2){
-					acid.output(true);
-					return true;			
+			if(p){
+				if(!fric){
+					if(!(right1 + obj1.velX	< left2 || left1 + obj1.velX > right2 || bottom1 < top2 || top1 > bottom2)){
+						return true;
+					}
+					else if(right1 < left2 && left1 + obj1.velX > right2 || left1 > right2 && right1 + obj1.velX < left2){
+						return true;			
+					}
+					else{
+						return false;
+					}
 				}
 				else{
-					return false;
+					if(!(right1 + obj1.velX	< left2 || left1 + obj1.velX > right2 || bottom1 + 1 < top2 || top1 - 1 > bottom2)){
+						return true;
+					}
+					else{
+						return false;
+					}
 				}
 			}
 			else{
-				if(!(right1 + 1 < left2 || left1 - 1 > right2 || bottom1 + obj1.velY < top2 || top1 + obj1.velY > bottom2)){
-					return true;
+				if(!fric){
+					if(!(right1 < left2 || left1 > right2 || bottom1 + obj1.velY < top2 || top1 + obj1.velY > bottom2)){
+						return true;
+					}
+					else if(bottom1 < top2 && top1 + obj1.velY > bottom2 || top1 > bottom2 && bottom1 + obj1.velY < top2){
+						acid.output(true);
+						return true;			
+					}
+					else{
+						return false;
+					}
 				}
 				else{
-					return false;
-				}
-			}		
+					if(!(right1 + 1 < left2 || left1 - 1 > right2 || bottom1 + obj1.velY < top2 || top1 + obj1.velY > bottom2)){
+						return true;
+					}
+					else{
+						return false;
+					}
+				}		
+			}
 		}
 	}
 }
 
-acid.addChild = function(theChild){
+acid.addChild = function(theChild, camera){
 	if(theChild.hasAttribute("src")){
 		if(!theChild.xs){
 			theChild.xs = 0;
@@ -201,7 +275,17 @@ acid.addChild = function(theChild){
 		if(!theChild.ys){
 			theChild.ys = 0;
 		}
-		theChild.onload = function(){displayList.push(theChild);};
+		if(camera){
+			acid.camera.id = displayList.length + 2;
+			theChild.camera = true;
+		}
+		theChild.onload = function(){
+			if(!theChild.nwidth){
+				theChild.nwidth = theChild.naturalWidth;
+				theChild.nheight = theChild.naturalHeight;
+			}
+			displayList.push(theChild);
+		};
 	}
 /*	else if(theChild.canvas){
 		imageNull = new Image();
@@ -225,24 +309,22 @@ acid.removePhysics = function(theChild){
 	}
 }
 
-acid.addPhysics = function(thePhysics, grav, fric, isSolid, bounce, isStatic){
+acid.addPhysics = function(thePhysics, grav, fric, isSolid, bounce, isStatic, plat){
 	thePhysics.gravity = true;
-	thePhysics.friction = 0.1;
+	thePhysics.friction = 0.08;
 	thePhysics.solid = true;
 	thePhysics.freeze = false;
 	thePhysics.bounce = 5;
+	thePhysics.platform = false;
 	physicsList.push(thePhysics);
 	if(grav != undefined){thePhysics.gravity = grav;}
 	if(fric != undefined){thePhysics.friction = fric;}
 	if(isSolid != undefined){thePhysics.solid = isSolid;}
 	if(bounce != undefined){thePhysics.bounce = bounce;}
 	if(isStatic != undefined){thePhysics.freeze = isStatic;}
+	if(isStatic != undefined){thePhysics.platform = plat;}
 	thePhysics.velX = 0;
 	thePhysics.velY = 0;
-	if(!thePhysics.nwidth){
-		thePhysics.nwidth = thePhysics.naturalWidth;
-		thePhysics.nheight = thePhysics.naturalHeight;
-	}
 	thePhysics.boundBox = [0,0,thePhysics.nwidth,thePhysics.nheight];
 }
 
--- script.js
+++ script.js
@@ -1,34 +1,30 @@
 acid.init = function(){
 	acid.debug = true;
-	ground = new Image();
-	ground.src = "ground.png";
+	ground = acid.rect("line",442,33,"red");
 	ground.ys = 400;
 	acid.addChild(ground);
-	acid.addPhysics(ground, false, 0.5, false, 5, true);
+	acid.addPhysics(ground, false, 0.08, true, 5, true, true);
 	wall = acid.rect("fill", 10, 500, "blue");
 	wall.xs = 400;
 	acid.addChild(wall);
-	acid.addPhysics(wall, false, 0.5, false, 5, true);
+	acid.addPhysics(wall, false, 0.08, false, 5, true);
 	box = acid.rect("line", 20, 20, "red");
-	acid.addChild(box);
-	acid.addPhysics(box, true, 0.1);
+	acid.addChild(box, true);
+	acid.addPhysics(box, true);
+	acid.camera.margin = 30;
 }
 
 acid.update = function(){
-}
-
-acid.keydown = function(evt){
-	switch(evt){
-		case 37:
-			box.velX -= 5;
-		break;
-		case 39:
-			box.velX += 5;
-		break;
-		case 38:
-			box.velY -= 5;
-		break;
-		default:
-			//
+	if(acid.keyboard.right){
+		box.velX += 0.4;
+	}
+	if(acid.keyboard.left){
+		box.velX -= 0.3;
+	}
+	if(acid.keyboard.up){
+		box.velY -= 0.4;
+	}
+	if(acid.keyboard.down){
+		box.velY += 0.3;
 	}
 }
